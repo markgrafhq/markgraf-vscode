d6a373ee21f899f1e3609bb3ed0a4f8676d122b3
Mark <mark.eibes@gmail.com>
2026-06-29 21:56:46 +0200

    Replace Markgraf fences in Markdown preview script

--- markdown/preview.js
+++ markdown/preview.js
@@ -1,5 +1,25 @@
 (function () {
+  const encodeBase64 = text => btoa(unescape(encodeURIComponent(text)));
+
+  const replaceFences = () => {
+    for (const code of document.querySelectorAll('pre > code.language-markgraf, pre > code.lang-markgraf')) {
+      const pre = code.parentElement;
+      if (!pre || pre.dataset.markgrafReplaced === "1") {
+        continue;
+      }
+
+      pre.dataset.markgrafReplaced = "1";
+      const preview = document.createElement("div");
+      preview.className = "markgraf-markdown-preview markgraf-embed";
+      preview.setAttribute("data-markgraf", "");
+      preview.setAttribute("data-markgraf-src-b64", encodeBase64(code.textContent || ""));
+      preview.setAttribute("data-markgraf-titles", "false");
+      pre.replaceWith(preview);
+    }
+  };
+
   const mount = () => {
+    replaceFences();
     if (window.markgraf && typeof window.markgraf.mountAll === "function") {
       window.markgraf.mountAll(document);
     }
